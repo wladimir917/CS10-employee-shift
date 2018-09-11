0fdb854638b3950dcbfbee3319bde42c6e406858
Justin Kaseman <justinkaseman@live.com>
2018-09-11 10:29:52 -0700

    Add routing and redux skeletons

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "../styles/App.css";
+
+// import {
+//   Billing,
+//   Calendar,
+//   Dashboard,
+//   Employees,
+//   Landing,
+//   Settings,
+//   Signin,
+// } from "./Pages";
+import Landing from "./Pages/Landing";
+
+// TODO: Add routing
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact path="/" component={Landing} />
       </div>
     );
   }
---
+++ front-end/src/components/Pages/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <p>hi</p>
+    </div>
+  );
+};
+
+export default Landing;
