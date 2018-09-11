987348a5c436c40fca9e19c1056dd51fafb84950
Obo Agboghidi <oagboghi2@gmail.com>
2018-09-11 14:39:32 -0700

    built skeleton of login page

--- front-end/src/components/Pages/Signin.js
+++ front-end/src/components/Pages/Signin.js
@@ -1,8 +1,59 @@
-import React, { Component } from "react";
+import React, { Component } from "react"; 
+import {Card, CardTitle, CardBody} from 'reactstrap';
+import '../../styles/signin.css';
+
+
+
 
 class Signin extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+  submitHandler = e => { //submit the form
+    e.preventDefault();
+
+  }
+
+  inputChangeHandler = event => {
+    const { name, value } = event.target;
+    this.setState({ [name] : value });
+  }
+
+
   render() {
-    return <div />;
+    return (
+      <Card className='card'>
+        <form onSubmit={this.submitHandler}>
+        < CardBody className='card-body' >
+          <div>
+            <CardTitle>Email Address</CardTitle>
+            <input
+            value = {this.state.email}
+            onChange = {this.inputChangeHandler}
+            name = 'email'
+            type = 'text'
+            />
+          </div>
+          <div>
+            <CardTitle>Password</CardTitle>
+            <input 
+            value = {this.state.password}
+            onChange = {this.inputChangeHandler}
+            name = 'password'
+            type = 'text'
+            />
+          </div>
+          <div>
+            <button type = 'submit'>Sign in</button>
+          </div>
+          </CardBody>
+        </form>
+      </Card>
+    );
   }
 }
 
