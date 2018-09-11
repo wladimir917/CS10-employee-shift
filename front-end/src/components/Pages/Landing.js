d5e01a36039972b3d7ab1702e33c5952155d40dd
kyle <kauzlarich.kyle@gmail.com>
2018-09-11 13:27:04 -0700

    formated

--- front-end/src/components/Pages/Landing.js
+++ front-end/src/components/Pages/Landing.js
@@ -4,24 +4,22 @@ import '../../styles/LandingRef.css'
 const Landing = () => {
   return (
     <div className="container">
-    <div className="background-holder">
-      <div className="nav-bar">
-      <button>Sign up</button>
-      <button>Sign in</button>
+      <div className="background-holder">
+        <div className="nav-bar">
+          <button>Sign up</button>
+          <button>Sign in</button>
+        </div>
+        <span className="welcome-text">Welcome</span>
       </div>
-      <span className="welcome-text">Welcome</span>
-    </div>
-    <div className="schedule-button">
-    <button className="button-button">Schedule Now
+      <div className="schedule-button">
+        <button className="button-button">Schedule Now
     </button>
-    </div>
-    <div 
-    
-    className="footer">
-    <footer>
-    <span>Copyright 2018</span>
-    </footer>
-    </div>
+      </div>
+      <div className="footer">
+        <footer>
+          <span>Copyright 2018</span>
+        </footer>
+      </div>
 
     </div>
   );
