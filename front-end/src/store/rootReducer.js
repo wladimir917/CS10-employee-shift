f7ef85eae383ef1fd6f3d9f9cb9cea73b8ea2d22
Justin Kaseman <justinkaseman@live.com>
2018-09-11 13:46:03 -0700

    rootReducer imports

--- front-end/src/store/rootReducer.js
+++ front-end/src/store/rootReducer.js
@@ -1,6 +1,23 @@
 import { combineReducers } from "redux";
-// import reducers
+
+import user from "./user/reducer.js";
+import timeOff from "./timeOff/reducer.js";
+import shift from "./shift/reducer.js";
+import hourOfOperation from "./hourOfOperation/reducer.js";
+import employer from "./employer/reducer.js";
+import employee from "./employee/reducer.js";
+import contact from "./contact/reducer.js";
+import calendarDay from "./calendarDay/reducer.js";
+import availability from "./availability/reducer.js";
 
 export const rootReducer = combineReducers({
-  /// reducers
+  user,
+  timeOff,
+  shift,
+  hourOfOperation,
+  employee,
+  employer,
+  contact,
+  calendarDay,
+  availability,
 });
