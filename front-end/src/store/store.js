8a96820817ecdb979c0e72a10c99b071f3a82fd3
Justin Kaseman <justinkaseman@live.com>
2018-09-11 13:19:14 -0700

    Add data model assets

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -3,10 +3,5 @@ import { rootReducer } from "./rootReducer.js";
 import thunk from "redux-thunk";
 
 export default function configureStore() {
-  return createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
-  );
+  return createStore(rootReducer, applyMiddleware(thunk));
 }
