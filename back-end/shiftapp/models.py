b6a057a5fdd90e266aaa5ae26134e50f70cfb66f
Brandon Hopper <brandonhopper0@gmail.com>
2018-09-12 18:43:21 -0400

    --added cors headers and OAUTH2 class in provider

--- back-end/shiftapp/models.py
+++ back-end/shiftapp/models.py
@@ -2,55 +2,69 @@ from django.db import models
 from uuid import uuid4
 from django.contrib.auth.models import User
 
-
 class Employer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     name = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    telephone = models.CharField(max_length=14)
 
+    def __str__(self):
+      return self.name
 
-class Contact(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    telephone = models.CharField(max_length=10)
-    telephone2 = models.CharField(max_length=10)
-
-
-class Employee(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-   
-   
 class HourOfOperation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
     start_hour = models.TimeField(auto_now=False, auto_now_add=False)
     end_hour = models.TimeField(auto_now=False, auto_now_add=False)
-
-
-class Shift(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    start_hour = models.TimeField(auto_now=False, auto_now_add=False)
-    end_hour = models.TimeField(auto_now=False, auto_now_add=False)
-    status = models.BooleanField(default=True)
-  
-    
 
 class CalendarDay(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    hour_of_operation = models.ForeignKey(HourOfOperation, on_delete=models.CASCADE)
+
     date = models.DateField(auto_now_add=False)
-    status = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_holiday = models.BooleanField(default=False)
+    is_weekend = models.BooleanField(default=False)
     
-
-class Availability(models.Model):
+class Employee(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    date = models.DateField(auto_now_add=False)
-    start_hour = models.TimeField(auto_now=False, auto_now_add=False)
-    end_hour = models.TimeField(auto_now=False, auto_now_add=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
 
+    telephone = models.CharField(max_length=14)
 
 class RequestedTimeOff(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
     date = models.DateField(auto_now_add=False)
     reason = models.CharField(max_length=200)
-    status = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=False)
+    
+class Day(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+    name = models.CharField(max_length=15)
+    abbreviation_name = models.CharField(max_length=1)
+
+    def __str__(self):
+      return self.name
+      
+class Availability(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
+    start_hour = models.TimeField(auto_now=False, auto_now_add=False)
+    end_hour = models.TimeField(auto_now=False, auto_now_add=False)
+    
+class Shift(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    calendarday = models.ForeignKey(CalendarDay, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+    start_hour = models.TimeField(auto_now=False, auto_now_add=False)
+    end_hour = models.TimeField(auto_now=False, auto_now_add=False)
+    status = models.BooleanField(default=True)
