eb27d11a16fb1fa970c0d288f26072f4e074cf26
Obo Agboghidi <oagboghi2@gmail.com>
2018-09-12 10:39:56 -0700

    Worked on skeleton of settings page. Sans CSS

--- front-end/src/components/Pages/Settings.js
+++ front-end/src/components/Pages/Settings.js
@@ -1,8 +1,70 @@
 import React, { Component } from "react";
 
 class Settings extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      email: "",
+      phone: "",
+      old_password: "",
+      new_password: ""
+    }
+  }
+
+  inputChangeHandler = event => {
+    console.log("state text", event.target)
+    const {email, phone, old_password, new_password } = event.target;
+    this.setState({ [name] : value })
+  }
+
   render() {
-    return <div />;
+    <form onSubmit={this.submitForm}>
+      <div>
+        <label>Email</label>
+        <input
+        value = {this.state.email}
+        onChange = {this.inputChangeHandler}
+        name = 'email'
+        type = 'text'
+        />
+      </div>
+      < div >
+      <label> Phone</label>
+        <input
+      value = {
+        this.state.email
+      }
+      onChange = { this.inputChangeHandler}
+      name = 'email'
+      type = 'text' 
+        />
+      </div>
+
+      <input type="checkbox" name="msg" value="Email" />
+      <input type="checkbox" name="msg" value="Email" />
+
+      <div>
+        <label>Old Password</label>
+        <input
+        value = {this.state.old_password}
+        onChange = {this.inputChangeHandler}
+        name = 'old_password'
+        type = 'password'
+        />
+      </div>
+      <div>
+        <label>New Password</label>
+        <input
+        value = {this.state.new_password}
+        onChange = {this.inputChangeHandler}
+        name = 'new_password'
+        type = 'password'
+        />
+      </div>
+      <div>
+        <button type = 'submit'>Save</button>
+      </div>
+    </form>
   }
 }
 
