d0fb85e4ac9c085e29bb73be435ebb45e2cb5f3d
Justin Kaseman <justinkaseman@live.com>
2018-09-12 11:11:20 -0700

    login action and login component refactor for redux

--- front-end/src/components/Pages/Signin.js
+++ front-end/src/components/Pages/Signin.js
@@ -1,52 +1,53 @@
-import React, { Component } from "react"; 
-import {Card, CardTitle, CardBody} from 'reactstrap';
-import '../../styles/signin.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Card, CardTitle, CardBody } from "reactstrap";
+import "../../styles/signin.css";
+
+import { signin } from "../../store/User/actions.js";
 
 class Signin extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      email: "",
-      password: ""
-    }
-  }
-  submitHandler = e => { //submit the form
+  state = {
+    email: "",
+    password: "",
+  };
+
+  submitHandler = e => {
     e.preventDefault();
+    this.props.signin(this.state.email, this.state.password);
+  };
 
-  }
-
-  inputChangeHandler = event => { // render user input
+  inputChangeHandler = event => {
+    // render user input
     const { name, value } = event.target;
-    this.setState({ [name] : value });
-  }
-
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
-      <Card className='card'>
+      <Card className="card">
         <form onSubmit={this.submitHandler}>
-        < CardBody className='card-body' >
-          <div>
-            <CardTitle>Email Address</CardTitle>
-            <input
-            value = {this.state.email}
-            onChange = {this.inputChangeHandler}
-            name = 'email'
-            type = 'text'
-            />
-          </div>
-          <div>
-            <CardTitle>Password</CardTitle>
-            <input 
-            value = {this.state.password}
-            onChange = {this.inputChangeHandler}
-            name = 'password'
-            type = 'text'
-            />
-          </div>
-          <div>
-            <button type = 'submit'>Sign in</button>
-          </div>
+          <CardBody className="card-body">
+            <div>
+              <CardTitle>Email Address</CardTitle>
+              <input
+                value={this.state.email}
+                onChange={this.inputChangeHandler}
+                name="email"
+                type="text"
+              />
+            </div>
+            <div>
+              <CardTitle>Password</CardTitle>
+              <input
+                value={this.state.password}
+                onChange={this.inputChangeHandler}
+                name="password"
+                type="text"
+              />
+            </div>
+            <div>
+              <button type="submit">Sign in</button>
+            </div>
           </CardBody>
         </form>
       </Card>
@@ -54,4 +55,28 @@ class Signin extends Component {
   }
 }
 
-export default Signin;
+const mapStateToProps = state => {
+  let errors = [];
+  if (state.user.errors) {
+    errors = Object.keys(state.user.errors).map(field => {
+      return { field, message: state.user.errors[field] };
+    });
+  }
+  return {
+    errors,
+    isAuthenticated: state.user.isAuthenticated,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signin: (username, password) => {
+      return dispatch(signin(username, password));
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Signin);
--- front-end/src/store/User/reducer.js
+++ front-end/src/store/User/reducer.js
@@ -1,10 +1,44 @@
 const initialState = {
-  // TODO: fill me
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  isLoading: true,
+  user: null,
+  errors: {},
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    // TODO: fill me
+    case "FETCHING_USER":
+      return { ...state, isLoading: true };
+
+    case "FETCHED_USER":
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.user,
+      };
+
+    case "SIGNIN_SUCCESS":
+      localStorage.setItem("token", action.data.token);
+      return {
+        ...state,
+        ...action.data,
+        isAuthenticated: true,
+        isLoading: false,
+        errors: null,
+      };
+
+    case "SIGNOUT_SUCCESS":
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        errors: action.data,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+      };
 
     case "ERROR":
       return { ...state, error: action.errorMessage };
