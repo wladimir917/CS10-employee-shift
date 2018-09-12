4500fb4573a03503a26e071a7da01818afae5a57
Obo Agboghidi <oagboghi2@gmail.com>
2018-09-12 11:56:51 -0700

    worked on basic styling. Fixed a few errors in settings page.

--- front-end/src/components/Pages/Settings.js
+++ front-end/src/components/Pages/Settings.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Card,CardTitle,CardBody } from 'reactstrap';
+import '../../styles/settings.css';
 
 class Settings extends Component {
   constructor(props){
@@ -13,59 +15,43 @@ class Settings extends Component {
 
   inputChangeHandler = event => {
     console.log("state text", event.target)
-    const {email, phone, old_password, new_password } = event.target;
+    const {name, value} = event.target;
     this.setState({ [name] : value })
   }
 
   render() {
-    <form onSubmit={this.submitForm}>
-      <div>
-        <label>Email</label>
-        <input
-        value = {this.state.email}
-        onChange = {this.inputChangeHandler}
-        name = 'email'
-        type = 'text'
-        />
-      </div>
-      < div >
-      <label> Phone</label>
-        <input
-      value = {
-        this.state.email
-      }
-      onChange = { this.inputChangeHandler}
-      name = 'email'
-      type = 'text' 
-        />
-      </div>
+    return <Card className="card">
+        <form onSubmit={this.submitForm}>
+          <CardBody className="card_body">
+            <div>
+              <label>Email</label>
+              <input value={this.state.email} onChange={this.inputChangeHandler} name="email" type="text" />
+            </div>
+            <div>
+              <label> Phone</label>
+              <input value={this.state.phone} onChange={this.inputChangeHandler} name="email" type="text" />
+            </div>
 
-      <input type="checkbox" name="msg" value="Email" />
-      <input type="checkbox" name="msg" value="Email" />
+            <label>email</label>
+            <input type="checkbox" name="msg" value="Email" />
+            <label>text</label>
+            <input type="checkbox" name="msg" value="text" />
 
-      <div>
-        <label>Old Password</label>
-        <input
-        value = {this.state.old_password}
-        onChange = {this.inputChangeHandler}
-        name = 'old_password'
-        type = 'password'
-        />
-      </div>
-      <div>
-        <label>New Password</label>
-        <input
-        value = {this.state.new_password}
-        onChange = {this.inputChangeHandler}
-        name = 'new_password'
-        type = 'password'
-        />
-      </div>
-      <div>
-        <button type = 'submit'>Save</button>
-      </div>
-    </form>
-  }
+            <div>
+              <label>Old Password</label>
+              <input value={this.state.old_password} onChange={this.inputChangeHandler} name="old_password" type="password" />
+            </div>
+            <div>
+              <label>New Password</label>
+              <input value={this.state.new_password} onChange={this.inputChangeHandler} name="new_password" type="password" />
+            </div>
+            <div>
+              <button type="submit">Save</button>
+            </div>
+          </CardBody>
+        </form>
+      </Card>;
+  }  
 }
 
 export default Settings;
